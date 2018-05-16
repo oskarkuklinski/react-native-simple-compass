5572fe2dc4103d54a9909fea230620352174b23e
Jordan Duncan <jrdndncn@gmail.com>
2018-05-16 15:17:32 +0100

    Remove override on createJSModules
    Support RN 0.47.0+

--- android/src/main/java/com/reactlibrary/RNSimpleCompassPackage.java
+++ android/src/main/java/com/reactlibrary/RNSimpleCompassPackage.java
@@ -16,7 +16,7 @@ public class RNSimpleCompassPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNSimpleCompassModule(reactContext));
     }
 
-    @Override
+    // @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
